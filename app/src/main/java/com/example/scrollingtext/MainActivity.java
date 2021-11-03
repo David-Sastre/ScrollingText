54d10e16c43e553a035423c2e431129fc1c85969
David <dsastref@cifpfbmoll.eu>
2021-11-03 13:26:03 +0100

    Challenge Activity. Task 13

--- app/src/main/java/com/example/scrollingtext/MainActivity.java
+++ app/src/main/java/com/example/scrollingtext/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.scrollingtext;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void Add_Comment(View view) {
+        Button but = findViewById(R.id.new_coment);
+        EditText e = (EditText) findViewById(R.id.article_comment);
+        TextView txt = (TextView) findViewById(R.id.article);
+        but.setText("Send");
+        e.setVisibility(view.VISIBLE);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (but.getText() == "Send"){
+                   String oldText = txt.getText().toString();
+                   String comment = e.getText().toString();
+                   count ++;
+                   txt.setText(oldText + "\n" + "\n" +"Comment_" +count+" : "+ comment + "\n");
+                   but.setText("Add Comment");
+                   e.setVisibility(view.INVISIBLE);
+               } else {
+                    e.setVisibility(View.VISIBLE);
+                    but.setText("Send");
+                }
+            }
+        });
     }
 }
